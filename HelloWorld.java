6d38a059d0d689a0695e03a0c4f9006dc123c020
Agente-707 <lupareja2025@gmail.com>
2026-08-01 10:31:51 -0500

    Comentarios en Java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,9 @@
+/*
+Introduccion: Hola Mundo
+Agente-707 
+ */
+
+//Hola mundo en Java
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
