806cedce2c1f419e189641d62d9f4582390c636b
Agente-707 <lupareja2025@gmail.com>
2026-08-03 09:48:23 -0500

    Agregando comentarios

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,19 @@
+// Este es un comentario en Java
+// Al igual que en C++ se utiliza // para escribir un comentario
+
 /*
-Introduccion: Hola Mundo
-Agente-707 
+Este es
+un comentario
+de varias lineas
  */
 
-//Hola mundo en Java
+// Hola mundo en Java
+// Nombre de la clase: HelloWorld
 public class HelloWorld {
+    // Sintaxsis inicial para ejecutar el programa
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); // System.out.println => Imprime el mensaje dentro del parentesis con un salto de lineal al final
+        
+        System.out.println("Hola, Mundo!");
     }
 }
