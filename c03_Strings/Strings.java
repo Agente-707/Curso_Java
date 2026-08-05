eb179e89d842fa67be8c8e1ae07a8db0a2996d28
Agente-707 <lupareja2025@gmail.com>
2026-08-05 11:31:35 -0500

    Strings en Java

---
+++ c03_Strings/Strings.java
@@ -0,0 +1,29 @@
+package c03_Strings;
+
+public class Strings {
+    public static void main(String[] args){
+        // Cadenas de texto
+
+        String user_name = "Agente-707";
+        var name = new String("Luis"); // Sintaxis redundante
+
+        // Concatenacion
+        System.out.println(user_name + " " + name);
+        System.out.println("Mi nombre de usuario es: " + user_name + " y mi nombre real es " + name);
+
+        // Length
+        System.out.println(name.length()); // => 4
+        System.out.println(user_name.length()); // => 10
+        // Devuelve la cantidad de caracteres del string
+
+        // charAt
+        // Devuelve el caracter en la posicion que le mandes
+        // la primera posicion de los strings en java es el 0
+        System.out.println(name.charAt(0)); // => L
+        System.out.println(name.charAt(1)); // => u
+        System.out.println(name.charAt(2)); // => i
+        System.out.println(name.charAt(3)); // => s
+        
+
+    }
+}
