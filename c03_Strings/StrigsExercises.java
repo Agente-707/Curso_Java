f68c6ba68c6c0829056d658d1658e732ae069136
Agente-707 <lupareja2025@gmail.com>
2026-08-06 12:17:03 -0500

    Ejercicios con strings

---
+++ c03_Strings/StrigsExercises.java
@@ -0,0 +1,30 @@
+package c03_Strings;
+
+public class StrigsExercises {
+    public class StringsExercises {
+        public static void main(String[] args){
+            //1. Concatena dos cadenas de texto.
+            String my_string = "Esta casa ";
+            String my_other_string = " 4 pisos";
+            System.out.println(my_string + "tiene" + my_other_string);
+
+            //2. Muestra la longitud de una cadena de texto.
+
+            //3. Muestra el primer y último carácter de un string.
+
+            //4. Convierte a mayúsculas y minúsculas un string.
+
+            //5. Comprueba si una cadena de texto contiene una palabra
+
+            //6. Formatea un string con un entero
+
+            //7. Elimina los espacios en blanco al principio y al final de un string
+
+            //8. Sustituye todos los espacios en blanco de un string por un guión (-).
+
+            //9. Comprueba si dos strings son iguales.
+
+            //10. Comprueba si dos strings tienen la misma longitud.
+        }
+    }
+}
