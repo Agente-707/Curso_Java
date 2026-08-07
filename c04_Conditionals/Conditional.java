cf92d638815e15df822484cb14f34407c9408c37
Luis Augusto <lupareja2505@gmail.com>
2026-08-07 10:26:35 -0500

    Switch en Java
    Added comments explaining conditionals and switch statement in Java.

--- c04_Conditionals/Conditional.java
+++ c04_Conditionals/Conditional.java
@@ -4,6 +4,7 @@ public class Conditional{
     public static void main(String[] args){
 
         // Condicionales
+        // Las condicionales en Java funcionan exactamente igual que en C++.
 
         var age = 19;
 
@@ -25,6 +26,33 @@ public class Conditional{
             System.out.println("El usuario acaba de cumplir 18 años");
         }else{
             System.out.println("El usuario es menor de edad");
+        }
+
+        // Switch
+        int today = 3;
+
+        switch (today){
+            case 1: // Primer caso 'today==1', si no se cumple continua buscando
+                System.out.println("Hoy es lunes");
+                break;
+            case 2:
+                System.out.println("Hoy es martes");
+                break;
+            case 3: // 'today==3' sí cumple!
+                System.out.println("Hoy es miercoles"); // este mensaje se imprime
+                break; // sale del Switch y omite los casos restantes 
+            case 4:
+                System.out.println("Hoy es jueves");
+                break;
+            case 5:
+                System.out.println("Hoy es viernes");
+                break;
+            case 6:
+                System.out.println("Hoy es sabado");
+                break;
+            case 7:
+                System.out.println("Hoy es domingo");
+                break;
         }
     }
 }
