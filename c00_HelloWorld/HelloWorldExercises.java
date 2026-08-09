f089068425d4b43b0b3b718d2b2f857ae3bac6eb
Luis Augusto <lupareja2505@gmail.com>
2026-08-09 11:46:46 -0500

    Add solutions to HelloWorldExercises
    Added solutions and comments for various exercises.

--- c00_HelloWorld/HelloWorldExercises.java
+++ c00_HelloWorld/HelloWorldExercises.java
@@ -1,13 +1,22 @@
 package c00_HelloWorld;
 public class HelloWorldExercises {
     public static void main(String[] args) {
+        /*
+        Estos son algunos ejercicios simples
+        para practicar.
+        */
+
+        
         // 1. Imprime un mensaje que diga tu nombre
-        System.out.println("Mi nombre es Agente-707"); // println imprime un mensaje y luego hace un salto de linea
+        // Solucion:
+        System.out.println("Mi nombre es Agente-707"); 
 
         // 2. Imprime dos lineas: "Hola" y luego "Mundo" con un solo println
-        System.out.println("Hola\nMundo"); // \n es un salto de linea
+        // Solución:
+        System.out.println("Hola\nMundo");
 
         //3. Crea un comentario en varias lineas.
+        // Solución:
         /* Este es 
            un comentario
            en varias
@@ -15,11 +24,13 @@ public class HelloWorldExercises {
         */
 
         //4. Imprime tu edad, tu color favorito y tu ciudad.
+        // Solución:
         System.out.println("Edad: 19");
         System.out.println("Color favorito: Verde");
         System.out.println("Ciudad: Lima");
 
         //5. Explora los diferentes System.err, System.out.
+        // Solución:
         System.err.println("Este es un mensaje de error"); // Esto imprime un mensaje de error en la consola
         System.out.println("Este es un mensaje con salto de linea");
         System.out.print("Este es un mensaje sin salto de linea /");
@@ -28,12 +39,14 @@ public class HelloWorldExercises {
         System.out.println(); // Esto imprime una linea vacia
 
         //6. Utiliza varios println para imprimir una frase.
+        // Solución:
         System.out.println("Esta");
         System.out.println("es");
         System.out.println("una");
         System.out.println("frase");
 
         //7. Imprime un diseño ASCII (Por ejemplo: Una cara feliz, un corazón, una casa, etc.)
+        // Solución:
         System.out.println("   -----   ");
         System.out.println("  /     \\  ");
         System.out.println(" |  O O  | ");
