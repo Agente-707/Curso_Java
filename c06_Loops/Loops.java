801f63f14def8c4aa875e64c9147e624efe3f8b1
Agente-707 <lupareja2025@gmail.com>
2026-08-15 11:05:32 -0500

    Loops en Java

---
+++ c06_Loops/Loops.java
@@ -0,0 +1,37 @@
+package c06_Loops;
+
+public class Loops {
+    public static void main(String[] args){
+
+        // Loops (o bucles)
+
+        // - for: Controlado por contador
+
+        for(int ind = 0; ind < 5; ind++){
+            System.out.println("Hola mundo!"); // Imprime "Hola mundo!" 5 veces
+        }
+
+        /*
+        Nota:
+        La estructura del for en Java
+        es exactamente igual que en C++
+        primero inicializamos el indice 
+        (int ind = 0), despues establecemos
+        sus limites (ind < 5), y por ultimo,
+        indicamos como va a variar en cada
+        vuelta (i++ aumenta de uno en uno).
+        */
+
+        for(int ind = 1; ind <= 5; ind+=2){
+            System.out.println("Hola mundo!"); // Imprime "Hola mundo!" 3 veces
+        }
+
+        // El indice aumenta de dos en dos y no puede ser mayor que 5.
+        // ind = 1   => "Hola mundo!"
+        // ind = 3   => "Hola mundo!"
+        // ind = 5   => "Hola mundo!"
+        // El bucle termina
+
+        
+    }
+}
