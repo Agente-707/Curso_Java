fb435271f4c9aa436e57b01c6f52a3c2aa6684b4
Luis Augusto <lupareja2505@gmail.com>
2026-08-15 09:30:29 -0500

    Add exercises for practicing data structures
    Added comments for exercises on data structures.

--- c05_Structures/StructuresExercises.java
+++ c05_Structures/StructuresExercises.java
@@ -3,6 +3,20 @@ package c05_Structures;
 public class StructuresExercises{
 
   public static void main(String args){
-    
+    /*
+    Algunos ejercicios simples de
+    las cuatro tipos estructuras 
+    para practicar
+    */
+
+    // 1. Crea un Array de 5 elementos e imprime su longitud.
+
+    // 2. Modifica uno de los valores del Array e imprime el valor del índice antes y despues de modificarlo.
+
+    // 3. Crea un ArrayList vacía.
+
+    // 4. Añade 4 valores al ArrayList y elimina uno a continuación.
+
+    // 5. Crea un HashSet con 2 valores diferentes.
   }
 }
