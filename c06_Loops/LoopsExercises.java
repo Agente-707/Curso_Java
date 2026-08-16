c0cacc468fd2375b9a206eccd2867998d9bfa37e
Luis Augusto <lupareja2505@gmail.com>
2026-08-16 11:24:04 -0500

    Add loop exercises comments to LoopsExercises.java
    Added comments outlining simple loop exercises.

--- c06_Loops/LoopsExercises.java
+++ c06_Loops/LoopsExercises.java
@@ -2,6 +2,20 @@ package c06_Loops;
 
 public class LoopsExercises{
   public static void main(String[] args){
+    /*
+    Algunos ejercicios simples
+    de bucles
+    */
+
+    // 1. Imprime los números del 1 al 10 usando while.
+
+    // 2. Usa do-while para mostrar todos los valores de un ArrayList.
+
+    // 3. Imprime los múltiplos de 5 del 1 al 50 usando for.
+
+    // 4. Recorre un Array de 5 números e imprime la suma total.
+
+    // 5. Usa un for para recorrer un Array y mostrar sus valores.
     
   }
 }
