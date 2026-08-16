bbfe11b44b5986fda283d7d2ccf9c401faac6f56
Agente-707 <lupareja2025@gmail.com>
2026-08-16 11:11:32 -0500

    Bucle while, do-while y controladores

--- c06_Loops/Loops.java
+++ c06_Loops/Loops.java
@@ -100,7 +100,105 @@ public class Loops {
             System.out.println(email.getValue()); // => Imprime todas los valores de los elementos
         }
 
+        System.out.println("------------------------");
 
+
+        // - while 
+
+        /*
+        Con el bucle while podemos realizar
+        repeticiones siempre y cuando se 
+        cumpla una condición
+        */
+
+        int index = 0;  // Definimos un índice
+        while (index < 5){  // Mientras el índice sea menor que 5, el sistema se repite
+            System.out.println("Hola, Java!");
+            index++; // índice aumenta
+        }
+
+        System.out.println("------------------------");
+
+        index = 0;
+        while (index < names.length){  // Mientras el índice sea menor que la longitud de 'names', el sistema se repite
+            System.out.println(names[index]); // Imprime cada elemento de 'names'
+            index++; // índice aumenta
+        }
+
+        System.out.println("------------------------");
+
+        index = 0;
+        boolean find = false;
+        while(!find){ // Mientras 'find' sea falso, el sistema se repite
+            System.out.println(names[index]);
+            if(names[index].equals("Agente-707")){ // si el elemento es "Agente-707", 'find' pasa a ser true y el bucle se corta
+                find = true;
+            }
+            index++; // índice aumenta
+        }
+
+        System.out.println("------------------------");
+
+
+        // - do-while
+
+        /*
+        Do-while es otro tipo de bucle
+        el cual ejecuta primero lo que
+        esta dentro del 'do' y despues 
+        revisa que se cumpla la condición
+        del 'while' antes de retomar el bucle.
+        */
+
+        index = 0;
+        do{ // Primero se ejecuta lo que esta dentro del 'do'
+            System.out.println("Hola, Java!"); 
+            index++;
+        } while (index < 0); // Luego revisa si se cumple la condición
+
+        /*
+        Nota:
+        
+        while: Primero revisa la condición,
+        luego ejecuta el sistema.
+
+        do-while: Primero ejecuta el sistema,
+        luego revisa la condición.
+        */
+
+
+        // Control de bucles
+
+        // - break
+
+        /*
+        El break nos expulsa del bucle
+        ignorando la condición o el 
+        número de iteraciones.
+        */
+
+        for (String name:names){
+            if(name.equals("Agente-707")){
+                break; // Si name = "Agente-707", salimos del bucle 
+            }
+            System.out.println(name);
+        }
+
+        // - continue
+
+        /*
+        El continue ignora todas
+        las instrucciones que esten
+        acontinuación de este, y sigue 
+        con la ejecución del bucle.
+        */
+
+        for(int i=0;i<5;i++){
+            if(i==3){
+                continue; // cuando i = 3 se va a saltar a la siguiente iteracion (no se imprime el 3)
+            }
+            System.out.println(i);
+        }
 
     }
 }
