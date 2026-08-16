1b38b8f9c7e1b133e676cb9eeaf9617ebaab61e2
Agente-707 <lupareja2025@gmail.com>
2026-08-16 12:20:31 -0500

    Mas ejercicios con soluciones

--- c06_Loops/LoopsExercises.java
+++ c06_Loops/LoopsExercises.java
@@ -1,6 +1,9 @@
 package c06_Loops;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class LoopsExercises{
   public static void main(String[] args){
@@ -69,14 +72,76 @@ public class LoopsExercises{
 
     
     // 6. Usa un for-each para recorrer un HashSet y un HashMap.
+    // Solución:
+
+    HashSet<String> juegos = new HashSet<>();
+    HashMap<String, Integer> frutas = new HashMap<>(); 
+
+    juegos.add("God of War");
+    juegos.add("Hollow Knight");
+    juegos.add("Roblox");
+    juegos.add("Stardew Valley");
+
+    frutas.put("uvas", 45);
+    frutas.put("manzanas", 12);
+    frutas.put("peras", 23);
+    frutas.put("platanos", 11);
+
+    for(String juego: juegos){
+      System.out.println(juego);
+    }
+
+    for(Map.Entry<String, Integer> fruta: frutas.entrySet()){
+      System.out.println(fruta.getKey());
+      System.out.println(fruta.getValue());
+    }
+
 
     // 7. Imprime los números del 10 al 1 (descendiente) con un bucle for.
+    // Solución:
+
+    for(index=10; index>=1; index--){
+      System.out.println(index);
+    }
+
 
     // 8. Usa continue para saltar los múltiplos de 3 del 1 al 20.
+    // Solución:
+
+    for(index = 1; index <= 20; index++){
+      if(index % 3 != 0){
+        System.out.println(index);
+      }
+    }
+
 
     // 9. Usa break para un bucle cuando encuentres un número negativo en un Array.
+    // Solución:
+
+    int[] Numbers = {3, 1, 4, -5, 2, 8};
+
+    for(index = 0; index < Numbers.length; index++){
+      if(Numbers[index] < 0){
+        break;
+      }
+
+      System.out.println(Numbers[index]);
+    }
+
 
     // 10. Crea un programa que calcule el factorial de un número dado.
+    // Solución:
+
+    int x = 5;
+    int fact = 1;
+
+    for(index = 1; index <= x; index++){
+      fact *= index;
+    }
+
+    System.out.println(String.format("El factorial de %d es: %d", x, fact));
+
+
     
   }
 }
