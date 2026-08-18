0e8dd8fa8f382da6ff68a15c6135f367a98f8bcf
Luis Augusto <lupareja2505@gmail.com>
2026-08-18 08:05:15 -0500

    Add function exercise comments in FunctionsExercises.java
    Added comments for function exercises in Java.

--- c07_Functions/FunctionsExercises.java
+++ c07_Functions/FunctionsExercises.java
@@ -2,6 +2,22 @@ package c07_Functions
 
 public class FuncionsExercises{
   public static void main (String[] args){
+
+    /*
+    Algunos ejercicios simples 
+    con funciones
+    */
+
+    // 1. Crea una función que imprima ¡Te doy la bienvenida al curso de Java desde cero!
+
+    // 2. Escribe una función que reciba un nombre como parámetro y salude a esa persona.
+
+    // 3. Establezca una función que reciba dos números enteros y devuelva su resta. 
+
+    // 4. Crea una función que calcule el cuadrado de un número (n x n).
+
+    // 5. Escribe una función que reciba un número y diga si es par o impar.
+
     
   }
 }
