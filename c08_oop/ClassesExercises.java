7d94e1a62dad4a91ce9c3b2eaec024f12654bc05
Luis Augusto <lupareja2505@gmail.com>
2026-08-18 10:31:27 -0500

    Add exercise comments for classes and methods
    Added comments for class and method exercises.

--- c08_oop/ClassesExercises.java
+++ c08_oop/ClassesExercises.java
@@ -5,6 +5,14 @@ public class ClassesExercises{
 
     // 1. Crea una clase Book con atributos title y author. Crea un objeto y muestra sus datos.
 
+    // 2. Crea una clase Dog con un método bark() que imprima su sonido.
+
+    // 3. Añade un constructor a la clase Book que reciba title y author.
+
+    // 4. Crea una clase Car con atributos brand y model y un método showData().
+
+    // 5. Crea una clase Student con atributo score y un método que diga si aprobó (mayor o igual a 60)
+
     
   }
 }
