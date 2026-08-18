d1e1a82b5342b3f0b0c3f1a1a32308dbef51a803
Agente-707 <lupareja2025@gmail.com>
2026-08-18 10:21:22 -0500

    agregando algunos comentarios y enseñando más propiedades

--- c08_oop/Person.java
+++ c08_oop/Person.java
@@ -17,8 +17,35 @@ public class Person {
     String name;
     int age;
 
+    // Constructor
+    public Person(String name, int age){ // con parametros
+        // Establecemos que el valor de los atributos es igual al de los parametros del constructor
+        this.name = name;
+        this.age = age;
+        /*
+        Nota:
+        Los atributos y los parametros del
+        constructor no necesariamente deben 
+        tener el mismo nombre
+
+        this.name (nombre del atributo) = name (nombre del parametro)
+        this.age (nombre del atributo) = age (nombre del parametro)
+        */
+    }
+
     // Métodos
     public void sayHello(){
         System.out.println("Hola, soy " + name + " y tengo " + age + " años.");
     }
+
+    /*
+    Nota:
+    Si el constructor no recibiera ningun
+    parametro - public Person(){} tendriamos
+    que asignarle los valores de sus parametros
+    (name y age) por medio de llamada de puntos
+    desde el archivo Classes
+    - person.name = "Luis"
+    - person.age = 19
+    */
 }
