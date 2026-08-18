07c41e7eb30510930fe7f92811e356c43c1ac15c
Luis Augusto <lupareja2505@gmail.com>
2026-08-18 10:26:53 -0500

    Create ClassesExercises with main method
    Added a new class 'ClassesExercises' with a main method to demonstrate creating a Book class.

---
+++ c08_oop/ClassesExercises.java
@@ -0,0 +1,10 @@
+package c08_oop;
+
+public class ClassesExercises{
+  public static void main(String[] args){
+
+    // 1. Crea una clase Book con atributos title y author. Crea un objeto y muestra sus datos.
+
+    
+  }
+}
