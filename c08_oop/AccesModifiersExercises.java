2cd9ebc81787141a4a3f41ffb2c69984c944065c
Luis Augusto <lupareja2505@gmail.com>
2026-08-20 08:00:26 -0500

    Add exercise descriptions for OOP concepts

--- c08_oop/AccesModifiersExercises.java
+++ c08_oop/AccesModifiersExercises.java
@@ -6,7 +6,11 @@ public class AccesModifiersExercises {
 
         // 2. Crea una clase Product con el atributo privado price. Añade el método setPrice(double price) que solo permita precios mayores a 0.
 
-        
+        // 3. Crea una clase BankAccount con el atributo privado balance. Implementa los métodos deposit(double amount) y withdraw(double amount) que validen las cantidades correctamente.
+
+        // 4. Crea una clase Book con el atributo privado title. Permite leerlo con el método getTitle() pero no modificarlo (sin setTitle()). El title debe asigarse solo por el constructor.
+
+        // 5. Crea una clase Temperature con el atributo privado celsius. El método setCelsius(double celsius) solo debe aceptar valores entre -100 y 100.
 
         
     }
