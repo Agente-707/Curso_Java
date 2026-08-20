3fc6651ac06dbd459c8debf79df8e91b2cc531bf
Luis Augusto <lupareja2505@gmail.com>
2026-08-20 07:51:21 -0500

    Add exercise tasks for Person and Product classes
    Added comments for exercise tasks related to access modifiers.

--- c08_oop/AccesModifiersExercises.java
+++ c08_oop/AccesModifiersExercises.java
@@ -2,6 +2,11 @@ package c08_oop;
 
 public class AccesModifiersExercises {
     public static void main(String[] args){
+        // 1. Crea una clase Person con atributos privados name y age. Usa los métodos getName(), setName(), getAge() y setAge() para asignar y mostrar valores desde otra clase.
+
+        // 2. Crea una clase Product con el atributo privado price. Añade el método setPrice(double price) que solo permita precios mayores a 0.
+
+        
 
         
     }
