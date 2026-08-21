f4cd883b4379e96ed1e6ae9adb657e9c7078b4d0
Agente-707 <lupareja2025@gmail.com>
2026-08-21 08:58:31 -0500

    Ejercicios de herencia en Java

---
+++ c08_oop/InheritanceExercises.java
@@ -0,0 +1,15 @@
+package c08_oop;
+
+public class InheritanceExercises {
+    public static void main(String[] args){
+
+        /*
+        Algunos ejercicios de Herencia
+        en POO
+        */
+
+        
+
+
+    }
+}
