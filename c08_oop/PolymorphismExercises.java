07c89a6aff4faceea54001c4dd837403728e6af4
Agente-707 <lupareja2025@gmail.com>
2026-08-23 10:53:46 -0500

    Solución del ejercicio 1 y 2

--- c08_oop/PolymorphismExercises.java
+++ c08_oop/PolymorphismExercises.java
@@ -1,5 +1,7 @@
 package c08_oop;
 
+import java.util.ArrayList;
+
 public class PolymorphismExercises{
      public static void main(String[] args){
           /*
@@ -9,7 +11,30 @@ public class PolymorphismExercises{
 
           // 1. Crea una clase Animal con el método makeSound(). Luego crea subclases Dog, Cat y Cow que sobrescriban ese método con sonidos diferentes. Llama al método desde una lista de Animal.
 
+          ArrayList<Animal> animales = new ArrayList<>();
+
+          animales.add(new Dog());
+          animales.add(new Cat());
+          animales.add(new Cow());
+
+          for(Animal animal:animales){
+               animal.makeSound();
+          } 
+
+
           // 2. Crea una clase Shape con el método calculateArea(). Luego implementa subclases Circle y Rectangle con sus propias fórmulas. Usa una lista de Shape para recorrer e imprimir el área de varias figuras.
+
+          ArrayList<Shape> formas = new ArrayList<>();
+
+          formas.add(new Circle(5));
+          formas.add(new Circle(14));
+          formas.add(new Rectangle(3,4));
+          formas.add(new Rectangle(20, 10));
+
+          for(Shape forma:formas){
+               System.out.println(forma.calcularArea());
+          }
+
 
           // 3. Crea una clase Printer con varios métodos print() sobrecargados que acepten diferentes tipos de parámetros (String, int, double). Llama a cada uno desde main.
 
@@ -17,6 +42,66 @@ public class PolymorphismExercises{
 
           // 5. Crea una clase Vehicle con un método start(). Luego crea Car, Bike y Truck que sobrescriban ese método. Recorre una lista ArrayList<Vehicle> para llamar a start() en cada uno.
           
+     }
+
+     public static class Animal{
+          public void makeSound(){
+               System.out.println("Algún sonido");
+          }
+     }
+
+     public static class Dog extends Animal{
+          @Override
+          public void makeSound(){
+               System.out.println("Woof");
+          }
+     }
+
+     public static class Cat extends Animal{
+          @Override
+          public void makeSound(){
+               System.out.println("Meow");
+          }
+     }
+
+     public static class Cow extends Animal{
+          @Override
+          public void makeSound(){
+               System.out.println("Muuu");
+          }
+     }
+
+     public abstract static class Shape{
+          public abstract double calcularArea();
+     }
+
+     public static class Circle extends Shape{
+          double radio;
+
+          public Circle(double radio){
+               this.radio = radio;
+          }
+
+          @Override
+          public double calcularArea(){
+               return(radio * radio * Math.PI);
+          }
+     }
+
+     public static class Rectangle extends Shape{
+          double width;
+          double height;
+
+          public Rectangle(double width, double height){
+               this.width = width;
+               this.height = height;
+          }
+
+          @Override
+          public double calcularArea(){
+               return(width*height);
+          }
+     
      }
     
 }
