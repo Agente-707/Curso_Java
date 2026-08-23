76973569d9789f035499411c6065de2eef4781cd
Luis Augusto <lupareja2505@gmail.com>
2026-08-23 12:44:17 -0500

    Implement abstract class with subclasses for animals
    Added abstract class 'Animal' and subclasses 'Dog' and 'Cat'. Demonstrated usage in main method.

--- c08_oop/Abstraction.java
+++ c08_oop/Abstraction.java
@@ -3,5 +3,37 @@ package c08_oop;
 public class Abstraction {
     public static void main(String[] args){
         
+        // Abstracción
+
+        // - Clase abstracta
+
+        
+        // var animal = new Animal(); => Error: La clase Animal es abstracta, no se puede instanciar
+        // animal.sleep();
+
+        var perro = new Dog();
+        perro.sleep();
+
+        var gato = new Cat();
+        gato.sleep();
+
+        
+    }
+
+    // - Clase abstracta
+
+    public static abstract class Animal{
+
+        public void sleep(){
+            System.out.println("El animal está durmiendo");
+        }
+    }
+
+    public static class Dog extends Animal{
+        
+    }
+
+    public static class Cat extends Animal{
+        
     }
 }
