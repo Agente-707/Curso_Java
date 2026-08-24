69a940a37392e5b40ecbf6ca3cb8112d5aef3e55
Luis Augusto <lupareja2505@gmail.com>
2026-08-24 07:56:43 -0500

    Create AbstractionExercises with Shape class example
    Added an abstract class Shape and instructions for subclasses.

---
+++ c08_oop/AbstractionExercises.java
@@ -0,0 +1,13 @@
+package c08_oop;
+
+public class AbstractionExercises{
+      public static void main(String[] args){
+    
+            /*
+            Algunos ejercicios de Abstraccion
+            en POO.
+            */
+
+            // 1. Crea una clase abstracta Shape con el método calculateArea(). Luego implementa dos subclases: Circle y Rectangle, y haz que cada una calcule su propia área.
+      }
+}
