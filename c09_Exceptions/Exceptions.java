920e2470b23713be86e1f47b28c616d3e36a2aa2
Luis Pareja <lupareja2505@gmail.com>
2026-08-25 09:52:24 -0500

    Control de excepciónes de Java

---
+++ c09_Exceptions/Exceptions.java
@@ -0,0 +1,39 @@
+package c09_Exceptions;
+
+public class Exceptions {
+    public static void main(String[] args){
+
+        /*
+        Si nosotros intentaramos ejecutar 
+        lo siguiente:
+
+        var result = 10 / 0; 
+        System.out.println(result); 
+        System.out.println("Fin"); 
+
+        =>
+        El programa no tiene ningun error de sintaxis,
+        sin embargo, al intentar ejecutar 'result'
+        nuestro programa se detiene y lanza una
+        'excepción' (no se puede dividir un número
+        entre cero)
+        */
+
+
+        // Manejo de excepciones
+
+
+        try {
+
+            var result = 10/0;
+            System.out.println(result);
+
+        } catch(Exception e){ // => Parametro e que guarda la información del error
+            System.out.println("Error: " + e);
+
+            // Si el código dentro de 'try' es una excepción se imprimirá "Error"
+        }
+
+        System.out.println("Fin"); // La ejecución del código continúa
+    }
+}
