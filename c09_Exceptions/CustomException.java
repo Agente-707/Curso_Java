d4212d09d78c4563cf89a17eb05c1c293eb13bed
Luis Augusto <lupareja2505@gmail.com>
2026-08-26 08:58:43 -0500

    Add constructor to CustomException class

--- c09_Exceptions/CustomException.java
+++ c09_Exceptions/CustomException.java
@@ -1,5 +1,8 @@
 package c09_Exceptions;
 
 public class CustomException extends Exception{
-      
+
+      public CustomException(String message){
+            super(message); // 'super' nos permite lanzar cualquier mensaje para nuestro excepción personalizada
+      }
 }
