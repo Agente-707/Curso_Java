c9567ca7608dcabf12f5521d0e41a5047c206831
Luis Augusto <lupareja2505@gmail.com>
2026-08-26 11:00:06 -0500

    Add exercises for exception handling in Java
    Added comments for exception handling exercises in Java.

--- c09_Exceptions/ExceptionExercises.java
+++ c09_Exceptions/ExceptionExercises.java
@@ -3,6 +3,21 @@ package c09_Exceptions;
 public class ExceptionExercises{
      public static void main(String[] args){
 
+          /*
+          Alguno ejercicios sobre excepciones
+          en Java
+          */
+
+          // 1. Divide dos números almacenados en dos variables. Maneja la division por cero con try-catch.
+
+          // 2. Crea un array con tres elementos e intenta acceder al índice 5. Captura el ArrayIndexOutOfBoundsException.
+
+          // 3. Crea una variable String nula e intenta imprimir su longitud. Maneja el NullPointerException.
+
+          // 4. Escribe una función que transforme texto a número. Usa try-catch para manejar entradas no válidas (NumberFormatException).
+
+          // 5. Escribe un programa con un bloque finally que se ejecute siempre, haya o no error.
+
        
      }
 }
