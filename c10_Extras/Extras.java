3d7a76a0fd1223c7376d03542d9a1b8cc9e7a0a6
Luis Augusto <lupareja2505@gmail.com>
2026-08-28 08:46:55 -0500

    Import Person and Classes in Extras.java
    Added imports for Person and Classes from c08_oop package.

--- c10_Extras/Extras.java
+++ c10_Extras/Extras.java
@@ -1,5 +1,9 @@
 package c10_Extras;
 
+import c08_oop.Person;
+import c08_oop.Classes;
+// import c08_oop.*; => para importar todas las clases de un paquete
+
 public class Extras {
     public static void main(String[] args){
 
@@ -22,6 +26,26 @@ public class Extras {
             si una variable es nula.
             */
         }
+
+        
+        // import
+
+        new Person("Luis", 19, "1233456");
+
+        /*
+        Nota:
+        Nosotros cuando queremos usar una
+        clase dentro de un paquete externo
+        a esta misma, es necesario importar
+        el paquete junto con el nombre de la
+        clase, en este caso: 'import c08_oop.Person'
+        para poder hacer uso de la clase 'Person'
+        fuera de otro paquete.
+        */
+
+        new Classes(); // Si queremos importar muchas clases provenientes de un mismo paquete podemos usar 'import c08_oop.*' (para este caso)
+
+        
         
     }
 }
