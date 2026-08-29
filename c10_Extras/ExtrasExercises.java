f82598d8b28daaa9c2e24518a294f61f3159a975
Luis Augusto <lupareja2505@gmail.com>
2026-08-29 09:05:36 -0500

    Add exercise comments to ExtrasExercises.java
    Added comments for extra exercises in Java.

--- c10_Extras/ExtrasExercises.java
+++ c10_Extras/ExtrasExercises.java
@@ -2,6 +2,20 @@ package c10_Extras;
 
 public class ExtrasExercises {
     public static void main(String[] args){
+        /*
+        Algunos ejercicios de extras
+        en Java.
+        */
+
+        // 1. Crea una variable tipo String como null y verifica y verifíca que no esté vacía antes de usarla.
+
+        // 2. Escribe un programa que lea el nombre y edad del usuario usando Scanner.
+
+        // 3. Declara una constante final llamada MAX_SCORE con valor 100 y muéstrala.
+
+        // 4. Crea una variable global message y otra local message dentro del método main(). Muestra ambas.
+
+        // 5. Usa import java.util.Scanner; para leer un número y mostrar si es positivo o negativo.
         
     }
 }
